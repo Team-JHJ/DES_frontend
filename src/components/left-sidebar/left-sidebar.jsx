32f7b2d8b8261af57fff9fda1176b79404dfa887
khanna01 <govmfkdtm@gmail.com>
2024-10-18 15:04:06 +0900

    leftsidebar routing

--- src/components/left-sidebar/left-sidebar.jsx
+++ src/components/left-sidebar/left-sidebar.jsx
@@ -1,9 +1,13 @@
 import styles from './left-sidebar.module.css'
 import deslogo from '../../assets/img/deslogo.png'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom'
 
 export default function LeftSidebar() {
     const navigate = useNavigate()
+    const pathname = useLocation().pathname
+    const params = useParams()
+    // console.log(pathname.pathname)
+    console.log(params)
 
     return (
         <div className={styles['leftsidebar-container']}>
@@ -28,65 +32,150 @@ export default function LeftSidebar() {
                     <div
                         className={`${styles['menu-item']} ${styles['menu-main']}`}
                     >
-                        <Link to="/">main</Link>
+                        <Link
+                            to="/"
+                            className={`${pathname === '/' ? styles['active'] : ''}`}
+                        >
+                            main
+                        </Link>
                     </div>
                     <div
                         className={`${styles['menu-item']} ${styles['menu-vpp']}`}
                     >
-                        <Link to="/vpp">vpp</Link>
+                        <Link
+                            to="/vpp"
+                            className={`${pathname === '/vpp' ? styles['active'] : ''}`}
+                        >
+                            vpp
+                        </Link>
                     </div>
                     <div className={`${styles['menu-item']} ${styles['']}`}>
-                        <Link to="">House1</Link>
+                        <Link
+                            to="/house/1"
+                            className={`${pathname === '/house/1' ? styles['active'] : ''}`}
+                        >
+                            House1
+                        </Link>
                         <div className={styles['items']}>
                             <span>
-                                <Link to="">Distribution Energy Resource</Link>
+                                <Link
+                                    to="/house/1/der"
+                                    className={`${pathname === '/house/1/der' ? styles['active'] : ''}`}
+                                >
+                                    Distribution Energy Resource
+                                </Link>
                             </span>
                             <span>
-                                <Link to="">Home Loads</Link>
+                                <Link
+                                    to="/house/1/home-loads"
+                                    className={`${pathname === '/house/1/home-loads' ? styles['active'] : ''}`}
+                                >
+                                    Home Loads
+                                </Link>
                             </span>
                             <span>
-                                <Link to="">Energy Management Device</Link>
+                                <Link
+                                    to="/house/1/emd"
+                                    className={`${pathname === '/house/1/emd' ? styles['active'] : ''}`}
+                                >
+                                    Energy Management Device
+                                </Link>
                             </span>
                             <span>
-                                <Link to="">Sustainability Metrics</Link>
+                                <Link
+                                    to="/house/1/sust-metrics"
+                                    className={`${pathname === '/house/1/sust-metrics' ? styles['active'] : ''}`}
+                                >
+                                    Sustainability Metrics
+                                </Link>
                             </span>
                         </div>
                     </div>
                     <div
                         className={`${styles['menu-item']} ${styles['menu-home2']}`}
                     >
-                        <Link to="">House2</Link>
+                        <Link
+                            to="/house/2"
+                            className={`${pathname === '/house/2' ? styles['active'] : ''}`}
+                        >
+                            House2
+                        </Link>
                         <div className={styles['items']}>
                             <span>
-                                <Link to="">DER</Link>
+                                <Link
+                                    to="/house/2/der"
+                                    className={`${pathname === '/house/2/der' ? styles['active'] : ''}`}
+                                >
+                                    DER
+                                </Link>
                             </span>
                             <span>
-                                <Link to="">Home Loads</Link>
+                                <Link
+                                    to="/house/2/home-loads"
+                                    className={`${pathname === '/house/2/home-loads' ? styles['active'] : ''}`}
+                                >
+                                    Home Loads
+                                </Link>
                             </span>
                             <span>
-                                <Link to="">Energy Management Device</Link>
+                                <Link
+                                    to="/house/2/emd"
+                                    className={`${pathname === '/house/2/emd' ? styles['active'] : ''}`}
+                                >
+                                    Energy Management Device
+                                </Link>
                             </span>
                             <span>
-                                <Link to="">Sustainability Metrics</Link>
+                                <Link
+                                    to="/house/2/sust-metrics"
+                                    className={`${pathname === '/house/2/sust-metrics' ? styles['active'] : ''}`}
+                                >
+                                    Sustainability Metrics
+                                </Link>
                             </span>
                         </div>
                     </div>
                     <div
                         className={`${styles['menu-item']} ${styles['menu-home3']}`}
                     >
-                        <Link to="">House3</Link>
+                        <Link
+                            to="/house/3"
+                            className={`${pathname === '/house/3' ? styles['active'] : ''}`}
+                        >
+                            House3
+                        </Link>
                         <div className={styles['items']}>
                             <span>
-                                <Link to="">DER</Link>
+                                <Link
+                                    to="/house/3/der"
+                                    className={`${pathname === '/house/3/der' ? styles['active'] : ''}`}
+                                >
+                                    DER
+                                </Link>
                             </span>
                             <span>
-                                <Link to="">Home Loads</Link>
+                                <Link
+                                    to="/house/3/home-loads"
+                                    className={`${pathname === '/house/3/home-loads' ? styles['active'] : ''}`}
+                                >
+                                    Home Loads
+                                </Link>
                             </span>
                             <span>
-                                <Link to="">Energy Management Device</Link>
+                                <Link
+                                    to="/house/3/emd"
+                                    className={`${pathname === '/house/3/emd' ? styles['active'] : ''}`}
+                                >
+                                    Energy Management Device
+                                </Link>
                             </span>
                             <span>
-                                <Link to="">Sustainability Metrics</Link>
+                                <Link
+                                    to="/house/3/sust-metrics"
+                                    className={`${pathname === '/house/3/sust-metrics' ? styles['active'] : ''}`}
+                                >
+                                    Sustainability Metrics
+                                </Link>
                             </span>
                         </div>
                     </div>
