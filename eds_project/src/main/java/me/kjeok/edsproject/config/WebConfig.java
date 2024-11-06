3ee5d6d175090aa1605120573b82b9e06df02670
김지혁 <kjeok00@gmail.com>
2024-11-06 12:08:49 +0900

    API endpoint modify

--- eds_project/src/main/java/me/kjeok/edsproject/config/WebConfig.java
+++ eds_project/src/main/java/me/kjeok/edsproject/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173") // 허용할 프론트엔드의 URL
+                .allowedOrigins("*") // 허용할 프론트엔드의 URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*"); // 모든 헤더 허용
     }
